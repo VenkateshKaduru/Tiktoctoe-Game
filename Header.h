961f7b774088d4da9ec11fb770d57729f45bc647
Venkatesk Kaduru <kaduru1990@gmail.com>
2020-11-25 13:40:54 +0100

    update

---
+++ Header.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef TICTOKTOE_H_
+#define TICTOKTOE_H_
+
+#include<vector>
+class functions {
+
+private:
+	std::vector<char> square;
+	int Player;
+	int choice;
+	char mark;
+public:
+	functions();
+	void display();
+	int choose();
+	int check();
+	int result(int i);
+
+};
+
+#endif // !TICTOKTOE
+
+
---
+++ main.cpp
@@ -0,0 +1,30 @@
+// tictoktoeGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include<iostream>
+#include"Header.h"
+
+int main()
+{
+Start: int i;
+	functions function;
+	do {
+		function.display();//For displaying the Tic Toc Toe board 
+		function.choose();//For Selecting the numbers in the board
+		i = function.check();// Checking the marks in Horizontally, Vertically, Diagonally  
+	} while (i == -1 );
+	function.display();
+	function.result(i);//Win or Draw
+
+	{
+		std::cout << "Do You want to play again: y / n " << std::endl;
+		char x;
+		std::cin >> x;
+		if (x == 'y') {
+			goto Start;
+		}
+		else
+			std::cout << "Quitting..." << std::endl;
+	}
+return 0;
+
+}
